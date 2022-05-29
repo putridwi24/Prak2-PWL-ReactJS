a7569b048e37f861d0e322318cd5fc0974f86b30
Your Name <you@example.com>
2022-05-29 14:32:52 +0700

    ubah

--- src/component/product/Product.jsx
+++ src/component/product/Product.jsx
@@ -3,7 +3,11 @@ import "./product.css"
 const Product = () => {
   return (
     <div className="p">
-      contoh 
+      <div className="p-browser">
+        <div className="p-circle"></div>
+        <div className="p-circle"></div>
+        <div className="p-circle"></div>
+      </div>
     </div>
   );
 }
